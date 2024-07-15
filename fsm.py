c736447015480be2de5962fe7e09515fa84716cb
isko18 <toksonbaevislam2004@gmail.com>
2024-07-15 17:15:10 +0600

    parsing

--- fsm.py
+++ fsm.py
@@ -1,6 +1,6 @@
 import logging 
 import asyncio
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
